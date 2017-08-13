ade9e5c0b57391d242cf0eb2a26ec3a96400215f
Dmitry <dmi3z@ayndex.ru>
2017-08-13 15:55:08 +0300

    Add departments in menu

---
+++ src/app/pages/department/department.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {} from './index';
+
+@NgModule({
+    declarations:[],
+    imports:[],
+    exports:[]
+})
+
+export class DepartmentModule{}
